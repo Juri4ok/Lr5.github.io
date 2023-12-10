fd17656a5befcd8b2fc5da662ff4bcfebd2ade7a
Juri4ok <131774233+Juri4ok@users.noreply.github.com>
2023-12-10 16:57:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var shoppingCartIcon = document.getElementById("shoppingCart");
+    var cartItemCount = document.getElementById("cartItemCount");
+
+    var cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
+
+    updateCartItemCount();
+
+    shoppingCartIcon.addEventListener("click", function () {
+        if (cartItems.length === 0) {
+            alert("Корзина пуста");
+        } else {
+            window.location.href = "cart/index.html";
+        }
+    });
+
+    document.querySelectorAll(".avl").forEach(function (button) {
+        button.addEventListener("click", function () {
+            var productName = button.parentElement.parentElement.querySelector('.prod a').innerText;
+            var productLink = button.parentElement.parentElement.querySelector('.prod a').href;
+            var productPrice = getPrice(button.parentElement.parentElement.querySelector('.price'));
+
+            var quantity;
+            validation();
+            function validation(){
+                quantity = prompt("Вкажіть кількість:");
+                const parsedInput = parseInt(quantity);
+                if (!isNaN(parsedInput) && parsedInput % 1 === 0) {
+                    addToCart(productLink, quantity, productName, productPrice);
+                    alert("Товар додано");
+                    updateCartItemCount();
+                } else {
+                    alert("Будь ласка, введіть коректне ціле число.");
+                    validation();
+                }
+            }
+        });
+    });
+
+    function updateCartItemCount() {
+        cartItemCount.innerText = getUniqueItemCount();
+    }
+
+    function addToCart(productLink, quantity, productName, productPrice) {
+        var item = {
+            link: productLink,
+            quantity: parseInt(quantity),
+            name: productName,
+            price: productPrice
+        };
+        cartItems.push(item);
+        localStorage.setItem("cartItems", JSON.stringify(cartItems));
+    }
+
+    function getUniqueItemCount() {
+        var uniqueItems = [];
+        cartItems.forEach(function (item) {
+            if (!uniqueItems.some(function (uniqueItem) {
+                return uniqueItem.link === item.link;
+            })) {
+                uniqueItems.push(item);
+            }
+        });
+        return uniqueItems.length;
+    }
+
+    function getPrice(element) {
+        var priceString = element.innerText.trim().replace("грн", "");
+        return parseFloat(priceString);
+    }
+});
